a517c7493abb47fe0bf962d3a35c1ecdf6790a95
lduarte1991 <lduarte1991@gmail.com>
2021-10-08 13:54:12 -0400

    added dummy (for now) login screen

---
+++ hxydra/src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col class="mb-4 mt-15" cols="12">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Hxydra
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          webapp formerly known as theSystem
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="6"
+        offset="3"
+      >
+        <v-form>
+          <v-card class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Login</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                        <v-form>
+                           <v-text-field
+                              prepend-icon="mdi-account"
+                              name="username"
+                              label="username"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field
+                              id="password"
+                              prepend-icon="mdi-lock"
+                              name="password"
+                              label="password"
+                              type="password"
+                           ></v-text-field>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" to="/">Login</v-btn>
+                     </v-card-actions>
+                  </v-card>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+
+    data: () => ({
+    }),
+  }
+</script>
