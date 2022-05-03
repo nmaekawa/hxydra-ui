a22745b195471f3abca3f697bb904484d0ab7aaa
lduarte1991 <lduarte1991@gmail.com>
2022-05-03 15:23:38 -0400

    download progress bar for reports download

--- hxydra/src/components/ReportList.vue
+++ hxydra/src/components/ReportList.vue
@@ -19,6 +19,30 @@
         </v-btn>
       </template>
     </v-snackbar>
+    <v-snackbar
+      v-model="downloadBox"
+      centered
+      timeout="-1"
+      color="deep-purple accent-4"
+      elevation="24"
+    >
+      {{ progressMessage }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="downloadBox = false"
+        >
+          Close
+        </v-btn>
+      </template>
+      <v-progress-linear
+        color="white"
+        :buffer-value="progressValue"
+        stream
+      ></v-progress-linear>
+    </v-snackbar>
     <v-card>
       <v-card-title>
         Report List
@@ -29,7 +53,14 @@
           v-for="(item, i) in reports"
           :key="i"
         >
-          <a :href="api_domain + item.url" :download="item.description + '.csv'" target="_blank">{{item.description}}</a>&nbsp;(<a :href="api_domain + item.url + '?format=json'" :download="item.description + '.json'" target="_blank">JSON</a>)
+          <h3 style="margin-right:20px;">{{item.description}}</h3>
+          <v-btn
+            @click="downloadUrl(api_domain + item.url + '?format=csv', item.description + '.csv')"
+            style="margin-right:10px"
+          ><v-icon>mdi-download</v-icon> CSV</v-btn>
+          <v-btn
+            @click="downloadUrl(api_domain + item.url + '?format=json', item.description + '.json')"
+          ><v-icon>mdi-download</v-icon> JSON</v-btn>
         </v-list-item>
       </v-list>
     </v-card>
@@ -46,8 +77,11 @@
       reports: [],
       api_domain: process.env.VUE_APP_KONDO_DOMAIN,
       api_url: process.env.VUE_APP_KONDO_API_URL + 'report/',
+      progressValue: 0,
       errorBox: false,
       errorMessage: '',
+      downloadBox: false,
+      progressMessage: '',
       test_reports: [{
         name: 'Courses Launching in 14 Days',
         link: 'https://google.com'
@@ -69,7 +103,6 @@
           self.api_url
         )
           .then(data => {
-            console.log(data)
             self.reports = data.data
           })
           .catch(function(e) {
@@ -79,6 +112,46 @@
             self.reports = self.test_reports
           });
       },
+      async downloadUrl(url) {
+        // should trigger message "Requesting report. Wait a moment..."
+        this.downloadBox = true
+        this.progressMessage = 'Requesting report. This may take a few minutes...'
+        this.progressValue = 0
+        if (!axios) {
+          return
+        }
+
+        await axios.get(
+          url,
+          {
+            onDownloadProgress: progressEvent => {
+              const total = progressEvent.total
+              const current = progressEvent.loaded
+
+              let percentCompleted = Math.floor(current / total * 100)
+              this.progressValue = percentCompleted
+              this.progressMessage = "Downloading Report (" + percentCompleted + "%)"
+            }
+          }
+        ).then(res => {
+          this.progressMessage = "Report Downloaded"
+          let filename_str = res.headers['content-disposition']
+          const re = /filename="(.*)"/
+          let filename = filename_str.match(re)[1];
+          let a = document.createElement('a')
+          var objurl = URL.createObjectURL(new Blob([res.data]));
+          if (filename.indexOf('.json') != -1) {
+            objurl = URL.createObjectURL(new Blob([JSON.stringify(res.data)], {type:'application/json'}));
+          }
+          a.href = objurl;
+          a.download = filename;
+          a.click();
+        }).catch(e => {
+          this.errorBox = true
+          this.errorMessage = "API could not be reached. Could not download report."
+            console.log(e)
+        })
+      }
     },
     mounted() {
       this.getReports();
